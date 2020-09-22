d919dc863101e2552bc75f5a02ca388750bee010
Julien Girardin <julien.girardin@enix.fr>
2020-09-22 14:54:58 +0200

    Unit-testing upgrades

--- tests/test_terraform.py
+++ tests/test_terraform.py
@@ -6,11 +6,11 @@ import tftest
 
 @pytest.fixture
 def spawn():
-  tf = tftest.TerraformTest(os.path.join(os.path.dirname(__file__), 'terraform'))
-  tf.setup(cleanup_on_exit=False)
-  tf.apply()
-  yield tf.output()
-  tf.destroy()
+    tf = tftest.TerraformTest(os.path.join(os.path.dirname(__file__), 'terraform'))
+    tf.setup(cleanup_on_exit=False)
+    tf.apply()
+    yield tf.output()
+    tf.destroy()
 
 
 @pytest.fixture
@@ -43,13 +43,32 @@ def prepared_vm(inventory, running_vm):
     assert docker.status == 'successful'
 
 
-def test_install(inventory, prepared_vm):
+def install(inventory, **kwargs):
     for playbook in ['00-apiserver-proxy.yml',
                      '01-site.yml']:
         result = ansible_runner.run(
             envvars={'ANSIBLE_HOST_KEY_CHECKING': 'false',
-                 'ANSIBLE_FORCE_COLOR': 'true'},
+                     'ANSIBLE_FORCE_COLOR': 'true'},
             inventory=inventory,
-            playbook=os.path.join(os.path.dirname(__file__), '../playbooks/', playbook)
+            playbook=os.path.join(os.path.dirname(__file__), '../playbooks/', playbook),
+            **kwargs
         )
-        assert result.status == 'successful'
+    return result
+
+
+def test_install(inventory, prepared_vm):
+    result = install(inventory)
+    assert result.status == 'successful'
+
+
+def test_upgrade(inventory, prepared_vm):
+    result = install(inventory)
+    ansible_runner.run(
+        envvars={'ANSIBLE_HOST_KEY_CHECKING': 'false',
+                 'ANSIBLE_FORCE_COLOR': 'true'},
+        inventory=inventory,
+        module='command',
+        module_args='kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml',
+        host_pattern='kube_control_plane[0]'
+    )
+    result = install(inventory, extravars={'kube_version': '1.18'})
