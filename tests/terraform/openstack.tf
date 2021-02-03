7e4bd08cce4e9ccfc1d76a5b6512b8ad92d64eb2
Julien Girardin <julien.girardin@enix.fr>
2021-02-03 14:42:04 +0100

    Bump terraform version and pinned openstack provider version

--- tests/terraform/openstack.tf
+++ tests/terraform/openstack.tf
@@ -1,2 +1,1 @@
-provider "openstack" {
-}
+provider "openstack" {}
--- tests/terraform/versions.tf
+++ tests/terraform/versions.tf
@@ -1,11 +1,9 @@
 terraform {
+  required_version = ">= 0.14.0"
   required_providers {
-    local = {
-      source = "hashicorp/local"
-    }
     openstack = {
-      source = "terraform-providers/openstack"
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
     }
   }
-  required_version = ">= 0.13"
 }
