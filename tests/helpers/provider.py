4c7b980b4915fc1f39cf2af46936e652238f59c2
Julien Girardin <julien.girardin@enix.fr>
2023-04-28 10:28:36 +0200

    Fix --keep-servers-after-fail option by retrieving test result inside fixture
    Thanks pytest doc for doc snippet

--- tests/helpers/provider.py
+++ tests/helpers/provider.py
@@ -1,7 +1,23 @@
 import os
+from typing import Dict
 
 import pytest
 import tenacity
+from pytest import CollectReport, StashKey
+
+# https://docs.pytest.org/en/latest/example/simple.html#making-test-result-information-available-in-fixtures
+phase_report_key = StashKey[Dict[str, CollectReport]]()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # store test results for each phase of a call, which can
+    # be "setup", "call", "teardown"
+    item.stash.setdefault(phase_report_key, {})[rep.when] = rep
 
 
 @pytest.fixture
@@ -22,11 +38,16 @@ def cluster_spawn(provider):
 
 @pytest.fixture
 def cluster(request, provider, operating_system):
+    keep_after_fail = request.config.getoption("keep_servers_after_fail")
     try:
         cluster_spawn(provider)
         yield provider.cluster()
-        if not request.config.getoption("keep_servers"):
+        report = request.node.stash[phase_report_key]
+        if "call" in report and report["call"].failed:
+            if not keep_after_fail:
+                provider.destroy()
+        elif not request.config.getoption("keep_servers"):
             provider.destroy()
     except Exception:
-        if not request.config.getoption("keep_servers_after_fail"):
+        if not keep_after_fail:
             provider.destroy()
