dfdee2268878afecce3f9fbe6ca3f11cefe43250
Julien Girardin <julien.girardin@enix.fr>
2023-06-28 10:24:23 +0200

    Use GP2 for CI (GP1 gonna be removed)

--- tests/terraform/openstack/control_plane.tf
+++ tests/terraform/openstack/control_plane.tf
@@ -6,7 +6,7 @@ resource "openstack_compute_servergroup_v2" "cp_group" {
 resource "openstack_compute_instance_v2" "control_plane" {
   name            = "${var.stem}-cp-${count.index + 1}"
   image_name      = var.image_name
-  flavor_name     = "GP1.S"
+  flavor_name     = "GP2.2"
   key_pair        = openstack_compute_keypair_v2.ssh_deploy.name
   security_groups = ["default", openstack_compute_secgroup_v2.kubeadm.name]
 
--- tests/terraform/openstack/workers.tf
+++ tests/terraform/openstack/workers.tf
@@ -6,7 +6,7 @@ resource "openstack_compute_servergroup_v2" "workers_group" {
 resource "openstack_compute_instance_v2" "workers" {
   name            = "${var.stem}-node-${count.index + 1}"
   image_name      = var.image_name
-  flavor_name     = "GP1.S"
+  flavor_name     = "GP2.2"
   key_pair        = openstack_compute_keypair_v2.ssh_deploy.name
   security_groups = ["default", openstack_compute_secgroup_v2.kubeadm.name]
 
